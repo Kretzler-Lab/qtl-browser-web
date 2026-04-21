a32b8eee322680e3c4c1fc08aab7c5935c156897
zwright <zwright@umich.edu>
2026-04-21 11:39:28 -0400

    IP-809: header changes

--- src/components/QTL/VariantInfoTable.tsx
+++ src/components/QTL/VariantInfoTable.tsx
@@ -89,17 +89,17 @@ export const VariantInfoTable: FC<{
       headerTooltip: "Minor allele frequency for this variant in the sub-cohort indicated. All subsequent columns are the number of samples having the stated genotype in each cohort"
     },
     {
-      headerName: "GG",
+      headerName: "REF/0",
       field: "ggPatients",
       sortable: true
     },
     {
-      headerName: "GA",
+      headerName: "HET/1",
       field: "gaPatients",
       sortable: true
     },
     {
-      headerName: "AA",
+      headerName: "ALT/2",
       field: "aaPatients",
       sortable: true
     }
